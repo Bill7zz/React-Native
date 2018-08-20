03c2660eba59a6644b95ba2e51aaebde4e6132b0
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2018-08-20 04:55:27 -0300

    Add RaceList to display each item with info

--- countdown/RaceList.js
+++ countdown/RaceList.js
@@ -1,10 +1,38 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, Text,  StyleSheet } from 'react-native';
+import RaceItem from './RaceItem';
 
-class RaceList extends Component{
-    render(){
-        return(
-            <Text>First Component</Text>
+const style = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#E6E6E6'
+    }
+});
+
+
+
+
+
+
+class RaceList extends Component {
+    state = {
+        speedways: []
+    }
+
+    componentDidMount() {
+        const speedways = require('./Data.json').speedways;
+        this.setState({ speedways });
+    }
+    render() {
+        return (
+
+            <FlatList
+            style={style.list}
+            data={this.state.speedways}
+            renderItem={({item}) => <RaceItem item={item}/>}
+            keyExtractor={item => item.id}
+            />
         );
     }
 }
