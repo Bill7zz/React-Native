7ff9128ffd1a71a89bc94b1f09656908915d764b
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2018-08-20 01:13:51 -0300

    Add RaceList Component

--- countdown/App.js
+++ countdown/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RaceList from './RaceList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hola dani, estoy corriendo mi primera app</Text>
-        <Text>con React-Native</Text>
-        
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <RaceList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ countdown/RaceList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+class RaceList extends Component{
+    render(){
+        return(
+            <Text>First Component</Text>
+        );
+    }
+}
+export default RaceList;
